2dbffc23307f5be85545e536b81a529cedafffab
sirosaba <overlook@outlook.jp>
2018-07-25 13:40:44 +0900

    初めてのコミット!

---
+++ Assets/Editor/PostBuilder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.Callbacks;
+
+public class PostBuilder
+{
+    [PostProcessBuildAttribute()]
+    public static void OnPostProcessBuild(BuildTarget i_buildTarget, string i_pathToBuiltProject)
+    {
+        if (i_buildTarget == BuildTarget.iOS)
+        {
+            OnPostProcessBuild_iOS(i_buildTarget, i_pathToBuiltProject);
+        }
+    }
+
+    private static void OnPostProcessBuild_iOS(BuildTarget i_buildTarget, string i_pathToBuiltProject)
+    {
+        var plistPath = System.IO.Path.Combine(i_pathToBuiltProject, "Info.plist");
+        var plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+
+        plist.root.SetString("NSCameraUsageDescription", "");
+
+        plist.WriteToFile(plistPath);
+    }
+}
---
+++ Assets/Script/Cam.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cam : MonoBehaviour {
+
+    public class TestCamera : MonoBehaviour
+    {
+
+        [SerializeField]
+        private int m_width = 1920;
+        [SerializeField]
+        private int m_height = 1080;
+        [SerializeField]
+        private RawImage m_displayUI = null;
+
+        private WebCamTexture m_webCamTexture = null;
+
+        private IEnumerator Start()
+        {
+            if (WebCamTexture.devices.Length == 0)
+            {
+                Debug.LogFormat("カメラがねえ!!");
+                yield break;
+            }
+
+            yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+            if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+            {
+                Debug.LogFormat("カメラだめだそうです");
+                yield break;
+            }
+
+            // とりあえず最初に取得されたデバイスを使ってテクスチャを作りますよ。
+            WebCamDevice userCameraDevice = WebCamTexture.devices[0];
+            m_webCamTexture = new WebCamTexture(userCameraDevice.name, m_width, m_height);
+
+            m_displayUI.texture = m_webCamTexture;
+
+            // さあ、撮影開始だ！
+            m_webCamTexture.Play();
+        }
+    }
+}
